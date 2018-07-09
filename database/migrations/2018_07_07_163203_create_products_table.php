da7c41aecc2fdb3924e7a37b9e7fdc15cf6b65ce
Unknown <nicollet.njora05@gmail.com>
2018-07-09 09:37:07 +0300

    Product Image

--- database/migrations/2018_07_07_163203_create_products_table.php
+++ database/migrations/2018_07_07_163203_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->text('name');
+            $table->text('product_image');
             $table->text('price');
             $table->timestamps();
         });
