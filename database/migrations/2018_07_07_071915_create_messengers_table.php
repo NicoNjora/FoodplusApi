5a90187c05ce0cd79ae9aa0e4dd4f710b8f57384
Unknown <nicollet.njora05@gmail.com>
2018-07-10 11:22:03 +0300

    Messengers

---
+++ database/migrations/2018_07_07_071915_create_messengers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessengersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messengers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('branch_id')->unsigned();
+            $table->timestamps();
+
+             $table->foreign('branch_id')->references('id')
+            ->on('branches')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messengers');
+    }
+}
--- database/migrations/2018_07_07_163447_create_deliveries_table.php
+++ database/migrations/2018_07_07_163447_create_deliveries_table.php
@@ -17,11 +17,14 @@ class CreateDeliveriesTable extends Migration
             $table->increments('id');
             $table->integer('status');
             $table->integer('order_id')->unsigned();  
-            $table->integer('payment_id')->unsigned();           
+            $table->integer('payment_id')->unsigned();
+            $table->integer('messenger_id')->unsigned();
             $table->timestamps();
 
             $table->foreign('order_id')->references('id')
             ->on('orders')->onDelete('cascade');
+            $table->foreign('messenger_id')->references('id')
+            ->on('messengers')->onDelete('cascade');
             $table->foreign('payment_id')->references('id')
             ->on('payment')->onDelete('cascade');
         });
